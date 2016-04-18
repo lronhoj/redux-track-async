216a959078a06584a73ac2138c6bf61569e9a0b9
Lars Rønhøj <lkr@arosii.dk>
2016-04-18 18:04:05 +0200

    converted index to es6

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports.ASYNC = require('./lib/middleware.js').ASYNC;
-module.exports.middleware = require('./lib/middleware.js').default;
-
-module.exports.reducer = require('./lib/reducer.js').default;
-module.exports.actions = require('./lib/actions.js');
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import middleware_, {ASYNC as ASYNC_} from './middleware.js';
+import reducer_ from './reducer.js';
+import * as actions_ from './actions.js';
+
+export const ASYNC = ASYNC_;
+export const middleware = middleware_;
+export const reducer = reducer_;
+export const actions = actions_;
